6a3b5c935c90a826fa7e53106a1db0df45f9b6f8
sahadat <shmanik14@yahoo.com>
2021-04-02 22:43:13 +0600

    delete spineer

--- src/components/ManageProduct/ManageProduct.js
+++ src/components/ManageProduct/ManageProduct.js
@@ -2,7 +2,6 @@ import React, { useState, useContext, useEffect } from 'react';
 import { paintContext } from '../../App';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Spinner from 'react-bootstrap/Spinner';
 import Table from 'react-bootstrap/Table';
 import Col from 'react-bootstrap/Col';
 import { useHistory } from 'react-router-dom';
@@ -74,9 +73,6 @@ const ManageProduct = () => {
                     </tr>
                 </thead>
                 <tbody>
-                {
-                    manageProducts.length === 0 && <Spinner animation="border" />                  
-                }
             {
                 manageProducts.map(paint => 
                 <tr>
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,7 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { paintContext } from '../../App';
 import Container from 'react-bootstrap/Container';
-import Spinner from 'react-bootstrap/Spinner';
 import Table from 'react-bootstrap/Table';
 
 const Orders = () => {
@@ -38,9 +37,6 @@ const Orders = () => {
                         </tr>
                     </thead>
                     <tbody>
-                    {
-                        orderedProducts.length === 0 && <Spinner animation="border" />                  
-                    }
                 {
                     orderedProducts.map(paint => 
                     <tr>
